a5ece6b9dec726b8fe17022d56ce1d170c646f4b
ravilella1234 <masters.selenium@gmail.com>
2022-06-03 19:13:38 +0530

    WebDriver Methods

---
+++ June2022_6PMSeleniumBatch/src/main/java/com/launcher/TC_004.java
@@ -0,0 +1,38 @@
+package com.launcher;
+
+public class TC_004 extends BaseTest 
+{
+
+	public static void main(String[] args) throws Exception 
+	{
+		init();
+		
+		launch("chromebrowser");
+		
+		navigateUrl("amazonurl");
+		
+		driver.manage().window().maximize();
+		
+		String title = driver.getTitle();
+		System.out.println(title);
+		
+		String url = driver.getCurrentUrl();
+		System.out.println(url);
+		
+		driver.manage().deleteAllCookies();
+		
+		driver.navigate().back();
+		
+		Thread.sleep(4000);
+		
+		driver.navigate().forward();
+		
+		Thread.sleep(4000);
+		
+		driver.navigate().refresh();
+		
+		//driver.close();
+		driver.quit();
+	}
+
+}

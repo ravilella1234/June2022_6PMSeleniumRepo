c0d5ac4ab760d88c9f1ca3b97de843da55d4f4c6
ravilella1234 <masters.selenium@gmail.com>
2022-06-21 19:50:00 +0530

    cSS Seclectors & examples

---
+++ June2022_6PMSeleniumBatch/src/main/java/com/launcher/Example2.java
@@ -0,0 +1,33 @@
+package com.launcher;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Example2 
+{
+
+	public static void main(String[] args)
+	{
+		String month = "October 2022";
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.goibibo.com/");
+		driver.findElement(By.xpath("//div[@class='sc-jIZahH lbNpRy']/div/p[@class='sc-dIouRR gyRYav fswWidgetTitle']")).click();
+		List<WebElement> months = driver.findElements(By.xpath("//div[@class='DayPicker-Month']/div[1]/div"));
+		
+		for(int i=0;i<months.size();i++)
+		{
+			if(!months.get(i).getText().contains(month))
+				driver.
+		}
+
+	}
+
+}

b35cef690132616befb75f0d9fb7bbe221689896
ravilella1234 <masters.selenium@gmail.com>
2022-07-22 19:36:36 +0530

    Excel Data Driven Day-1

---
+++ June2022_6PMSeleniumBatch/src/test/java/excelDataDriven/ExcelDataWriterByColumnName.java
@@ -0,0 +1,39 @@
+package excelDataDriven;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelDataWriterByColumnName 
+{
+
+	public static void main(String[] args) throws Exception 
+	{
+		FileInputStream fis = new FileInputStream("C:\\Users\\ravi\\Desktop\\testdata1.xlsx");
+		XSSFWorkbook wb = new XSSFWorkbook(fis);
+		XSSFSheet sheet = wb.getSheet("login");
+		XSSFRow row = sheet.getRow(0);
+		XSSFCell cell = null;
+		
+		int cellNum = 0;
+		for(int i=0;i<row.getLastCellNum();i++)
+		{
+			if(row.getCell(i).getStringCellValue().trim().equals("Result"))
+				cellNum = i;
+		}
+		
+		row = sheet.getRow(2);
+		cell = row.getCell(cellNum);
+		cell.setCellValue("skipped");
+		
+		FileOutputStream fos = new FileOutputStream("C:\\Users\\ravi\\Desktop\\testdata1.xlsx");
+		wb.write(fos);
+		wb.close();
+		fis.close();
+	}
+
+}
